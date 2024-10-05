2a4f4b227391539ee58a1bf6eb4acfe62836a30e
rapidx.rpx <rapidx.rpx@gmail.com>
2024-10-05 23:17:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Simulating loading screen
+window.onload = function() {
+  document.getElementById('loading').style.display = 'none';
+}
+
+// Function to connect wallet (dummy implementation)
+function connectWallet() {
+  alert('Wallet connected! You can now buy RPX.');
+}
